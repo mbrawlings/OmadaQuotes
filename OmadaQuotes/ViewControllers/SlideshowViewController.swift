9ec5d0703185d09d767bac1942c6688094f1c265
Matthew Rawlings <mbrawlings11@gmail.com>
2022-11-15 20:29:10 -0700

    asyncAfter added and looping thru data

--- OmadaQuotes/ViewControllers/SlideshowViewController.swift
+++ OmadaQuotes/ViewControllers/SlideshowViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SlideshowViewController: UIViewController {
 
     //MARK: - PROPERTIES
+    var characterIndex = 0
     
     
     //MARK: - OUTLETS
@@ -20,12 +21,35 @@ class SlideshowViewController: UIViewController {
     //MARK: - LIFECYCLES
     override func viewDidLoad() {
         super.viewDidLoad()
-        authorLabel.text = QuoteDataController.shared.quoteData[0].author
-        quoteLabel.text = QuoteDataController.shared.quoteData[0].quote
+        authorLabel.center.x = view.center.x // Place it in the center x of the view.
+        authorLabel.center.x -= view.bounds.width // Place it on the left of the view with the width = the bounds'width of the view.
+            // animate it from the left to the right
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseOut], animations: {
+            self.authorLabel.center.x += self.view.bounds.width
+                  self.view.layoutIfNeeded()
+            }, completion: nil)
+        present()
     }
     
     //MARK: - HELPER METHODS
 
-
+    func present() {
+        self.view.layoutIfNeeded()
+        let waitTime = Double(QuoteDataController.shared.quoteData[characterIndex].slideTransitionDelay)/1000
+        authorLabel.text = QuoteDataController.shared.quoteData[characterIndex].author
+        quoteLabel.text = QuoteDataController.shared.quoteData[characterIndex].quote
+        print("Character wait time \(waitTime)")
+        print("Character index \(characterIndex)")
+            DispatchQueue.main.asyncAfter(deadline: .now() + waitTime) {
+                // after time above, execute this block of code
+                if self.characterIndex == QuoteDataController.shared.quoteData.count-1 {
+                    self.characterIndex = 0
+                } else {
+                    self.characterIndex += 1
+                }
+                self.view.layoutIfNeeded()
+                self.present()
+        }
+    }
 
 }
