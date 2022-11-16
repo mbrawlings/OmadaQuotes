fafa743473f65ae708cd9e69c59119ef5a2fdc9d
Matthew Rawlings <mbrawlings11@gmail.com>
2022-11-15 19:36:55 -0700

    files added/organized

---
+++ OmadaQuotes/Model/QuoteData.swift
@@ -0,0 +1,8 @@
+//
+//  QuoteData.swift
+//  OmadaQuotes
+//
+//  Created by Matthew Rawlings on 11/15/22.
+//
+
+import Foundation
---
+++ OmadaQuotes/ModelControllers/QuoteDataController.swift
@@ -0,0 +1,8 @@
+//
+//  QuoteDataController.swift
+//  OmadaQuotes
+//
+//  Created by Matthew Rawlings on 11/15/22.
+//
+
+import Foundation
